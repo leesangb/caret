40fe095379bb99d6e60d409e4fccae97111084d1
Sangbin Lee <leesangbin@outlook.com>
2026-04-13 23:27:00 +0900

    fix: add react vite plugin

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,7 +1,9 @@
 import { resolve } from 'node:path'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     lib: {
       entry: resolve(import.meta.dirname, 'src/index.ts'),
